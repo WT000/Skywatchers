9e1fd39aa4ae324875940c054665608dd73abedd
4TASKW67 <4taskw67@solent.ac.uk>
2022-01-10 21:48:14 +0000

    Progress for the day

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+// Setup the routes across the app
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.get("/database", (req, res) => {
+    res.render("database");
+});
+
+app.get("/statistics", (req, res) => {
+    res.render("statistics");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
 });
 
 // Start the app
