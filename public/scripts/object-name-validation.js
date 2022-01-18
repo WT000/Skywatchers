05699b3c9e58acc72f974b85bb932b91aee2572a
4TASKW67 <4taskw67@solent.ac.uk>
2022-01-18 14:45:59 +0000

    Ensured users can edit the names of their own objects appropriately with AJAX verification (also detects objects they've made), error message improvements

--- public/scripts/object-name-validation.js
+++ public/scripts/object-name-validation.js
@@ -1,10 +1,10 @@
-document.getElementById("add-form").addEventListener("submit", e => {
+document.getElementById("object-form").addEventListener("submit", e => {
     e.preventDefault();
 });
 
 // Fairly simple AJAX to demonstrate why having an API is useful, it ensures there's no hashtags and
 // then uses the API to search for the item name
-const handleValidation = async () => {
+const handleValidation = async (editorId) => {
     let objectName = document.getElementById("name-input").value;
 
     if (objectName !== "") {
@@ -19,18 +19,37 @@ const handleValidation = async () => {
                 const searchResult = await rawSearchResult.json();
 
                 if (searchResult.numObjects == 0) {
-                    document.getElementById("add-form").submit();
+                    document.getElementById("object-form").submit();
                 } else {
-                    document.getElementById("error-results").innerHTML = "The object is already on the public database, consider making it private";
-                    document.getElementById("error-results").style.display = "inline-block";
-                    setTimeout(e=> {$('.alert').hide();}, 3000);
+                    // Check if the brought in editUser matches the uploader id, so we know that the user can attempt to edit the object
+                    // Only validaiton is done at this stage, so if someone were to inspect element to change another object, the server-side code would
+                    // detect it and not make any changes
+                    let objectId = document.getElementById("objectId").value;
+                    let allowed = false;
+
+                    if (editorId) {
+                        searchResult.objects.forEach(object => {
+                            if (object.uploader._id === editorId && object._id === objectId ) {
+                                document.getElementById("object-form").submit();
+                                allowed = true;
+                            };
+                        });
+                    };
+
+                    // Prevents the error from coming up a few seconds before editing
+                    if (!allowed) {
+                        document.getElementById("error-results").innerHTML = "The object is already on the public database, consider making it private";
+                        document.getElementById("error-results").style.display = "inline-block";
+                        setTimeout(e=> {$('.alert').hide();}, 3000);
+                    };
                 };
             
             } catch (e) {
+                console.log(e);
                 console.log("Something went wrong with the API");
             };
         } else {
-            document.getElementById("add-form").submit();
+            document.getElementById("object-form").submit();
         };
     };
 };
